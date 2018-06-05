d12874c456a892c3f7cde9cd01eca645969972c3
Onkar <er.onkarjanwa@gmail.com>
2018-06-05 15:30:09 +0530

    Changed class name

--- x_model_popup.js
+++ x_model_popup.js
@@ -3,6 +3,7 @@ var xModelPopup = function(options) {
     var xModelOptions = {
         id: '',
         css: {
+            overlay_opacity: 0,
             overlay_color: '',
             z_index: 10000,
             border: '',
@@ -10,7 +11,10 @@ var xModelPopup = function(options) {
             padding: '10px',
             background_color: '',
             shadow: '',
-            top: false
+            top: false,
+            left: false,
+            width: false,
+            height: false
         },
         classes: {
             overlay: '',
@@ -46,9 +50,15 @@ xModelPopup.prototype.init = function() {
     xModelContainerDiv.style.borderRadius = css.border_radius ? css.border_radius : '4px';
     xModelContainerDiv.style.backgroundColor = css.background_color ? css.background_color : '#fff';
     xModelContainerDiv.style.padding = css.padding ? css.padding : '10px';
-    xModelContainerDiv.style.webkitBoxShadow = css.boxShadow ? css.boxShadow : '0px 0px 5px 0px rgba(0,0,0,0.75)';
-    xModelContainerDiv.style.mozBoxShadow = css.boxShadow ? css.boxShadow : '0px 0px 5px 0px rgba(0,0,0,0.75)';
-    xModelContainerDiv.style.boxShadow = css.boxShadow ? css.boxShadow : '0px 0px 5px 0px rgba(0,0,0,0.75)';
+    xModelContainerDiv.style.webkitBoxShadow = css.shadow ? css.shadow : '0px 0px 5px 0px rgba(0,0,0,0.75)';
+    xModelContainerDiv.style.mozBoxShadow = css.shadow ? css.shadow : '0px 0px 5px 0px rgba(0,0,0,0.75)';
+    xModelContainerDiv.style.boxShadow = css.shadow ? css.shadow : '0px 0px 5px 0px rgba(0,0,0,0.75)';
+    if(this.modelOptions.css.width) {
+        xModelContainerDiv.style.width = this.modelOptions.css.width;
+    }
+    if(this.modelOptions.css.height) {
+        xModelContainerDiv.style.height = this.modelOptions.css.height;
+    }
 
     var xModelHeaderDiv = document.createElement("div");
     xModelHeaderDiv.setAttribute("class", 'x-model-header ' + options.classes.header);
@@ -74,6 +84,7 @@ xModelPopup.prototype.init = function() {
     xModeloverlayDiv.style.backgroundColor = css.overlay_color ? css.overlay_color : 'rgba(96, 125, 139, 0.46)';
     xModeloverlayDiv.style.left = 0;
     xModeloverlayDiv.style.top = 0;
+    xModeloverlayDiv.style.opacity = css.overlay_opacity;
     xModelDiv.append(xModelContainerDiv);
     xModelDiv.append(xModeloverlayDiv);
     document.getElementsByTagName("body")[0].append(xModelDiv);
@@ -99,11 +110,11 @@ xModelPopup.prototype.position = function() {
         w: model.offsetWidth,
         h: model.offsetHeight
     };
-
     var top = (bodyOffset.h - modelOffset.h) / 2;
     var left = (bodyOffset.w - modelOffset.w) / 2;
-    model.style.top = this.modelOptions.css.top === false ? top : this.modelOptions.css.top;
-    model.style.left = this.modelOptions.css.left === false ? left : this.modelOptions.css.left;
+   
+    model.style.top = !this.modelOptions.css.top ? top : this.modelOptions.css.top;
+    model.style.left = !this.modelOptions.css.left ? left : this.modelOptions.css.left;    
 }
 xModelPopup.prototype.open = function() {
     document.getElementById(this.modelOptions.id).setAttribute('style', 'display: block');
